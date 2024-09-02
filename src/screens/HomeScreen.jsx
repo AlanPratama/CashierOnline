269e2b708fcfe0c07c213674789ed43142b2466e
AlanPratama <pralan76@gmail.com>
2024-09-02 20:46:33 +0700

    [FINAL]

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -7,7 +7,7 @@ import {
   StatusBar,
   Text,
   TouchableOpacity,
-  View
+  View,
 } from "react-native";
 import { LineChart } from "react-native-chart-kit";
 import Animated, { FadeIn } from "react-native-reanimated";
@@ -16,8 +16,7 @@ import BottomNavigation from "../components/BottomNavigation";
 import BottomSheetUpdateStore from "../components/BottomSheetUpdateStore";
 
 export default function HomeScreen() {
-
-  const [store, setStore] = useState({})
+  const [store, setStore] = useState({});
   const [totalProfit, setTotalProfit] = useState(0);
   const [totalTransaction, setTotalTransaction] = useState(0);
 
@@ -26,7 +25,7 @@ export default function HomeScreen() {
   const [transactions, setTransactions] = useState([]);
 
   const db = useSQLiteContext();
-  const refRbSheet = useRef(null)
+  const refRbSheet = useRef(null);
 
   useEffect(() => {
     fetch();
@@ -43,16 +42,14 @@ export default function HomeScreen() {
 
   // INSERT INTO transactions (productId, codeTransaction, quantity, totalPrice) VALUES (1, 'ab12cd34', 1, 2000), (2, 'ab12cd34', 1, 1000), (3, 'bcde1234', 1, 5000), (4, 'klmn0213', 1, 10000), (5, 'opqrs1452', 1, 3000);
 
-  
-
   const fetch = async () => {
-    const storeStatement = await db.prepareAsync("SELECT * FROM stores LIMIT 1");
+    const storeStatement = await db.prepareAsync(
+      "SELECT * FROM stores LIMIT 1"
+    );
     const execStore = await storeStatement.executeAsync();
     const resStore = await execStore.getAllAsync();
     await storeStatement.finalizeAsync();
 
-    
-    
     console.log("resStore[0]: ", resStore[0]);
     setStore(resStore[0]);
 
@@ -101,13 +98,12 @@ export default function HomeScreen() {
       setTotalTransaction(resTr[0]["COUNT(DISTINCT codeTransaction)"]);
 
       console.log('resPr[0]["SUM(totalPrice)"]: ', resPr[0]["SUM(totalPrice)"]);
-      console.log('resTr[0]["COUNT(DISTINCT codeTransaction)"]: ', resTr[0]["COUNT(DISTINCT codeTransaction)"]);
-      
-
+      console.log(
+        'resTr[0]["COUNT(DISTINCT codeTransaction)"]: ',
+        resTr[0]["COUNT(DISTINCT codeTransaction)"]
+      );
 
       setTransactions(resTran);
-
-      
 
       // console.log("PROFIT: ", resPr[0]["SUM(totalPrice)"]);
       // console.log("TR: ", resTr[0]["COUNT(*)"]);
@@ -115,7 +111,7 @@ export default function HomeScreen() {
       // console.log("TRAN: ", resTran);
     } catch (error) {
       console.error("ERROR: ", error.message);
-    } 
+    }
   };
 
   const fetchWeeklyTransactions = async () => {
@@ -151,10 +147,10 @@ export default function HomeScreen() {
   const screenWidth = Dimensions.get("window").width;
 
   const data = {
-    // labels: weeklyTransactions.map(transaction => transaction.transaction_date), 
+    // labels: weeklyTransactions.map(transaction => transaction.transaction_date),
     datasets: [
       {
-        data: weeklyTransactions.map((transaction) => transaction.total_price), 
+        data: weeklyTransactions.map((transaction) => transaction.total_price),
       },
     ],
   };
@@ -176,8 +172,8 @@ export default function HomeScreen() {
   };
 
   const handleSheetStore = () => {
-    refRbSheet.current.open()
-  }
+    refRbSheet.current.open();
+  };
 
   const groupedTransactions = transactions.reduce((acc, transaction) => {
     const { codeTransaction } = transaction;
@@ -187,122 +183,144 @@ export default function HomeScreen() {
     acc[codeTransaction].push(transaction);
     return acc;
   }, {});
-  
-  const transactionsArray = Object.values(groupedTransactions);
-  
-  
 
-  
+  const transactionsArray = Object.values(groupedTransactions);
 
   return (
     <>
-    <ScrollView className="flex-1">
-    <StatusBar style="auto" />
-      <View className="mt-6">
-        <Animated.View entering={FadeIn.delay(300)} className="px-3 pb-2 flex flex-wrap flex-row justify-between items-center gap-2">
-          <TouchableOpacity onPress={handleSheetStore}>
-          <Text className="text-[18px] font-bold text-neutral-700">
-            {store.name}
-          </Text>
-          <Text className="text-sm font-semibold text-neutral-700">
-            Owner: {store.owner}
-          </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={handleRefresh} className="bg-blue-500 px-4 py-2 rounded-lg">
-            <Text className="text-white font-semibold text-[14px]"><Icon name="refresh" size={14} color={"#ffffff"} /> Refresh Data</Text>
-          </TouchableOpacity>
-        </Animated.View>
+      <ScrollView className="flex-1">
+        <StatusBar style="auto" />
+        <View className="mt-6">
+          <Animated.View
+            entering={FadeIn.delay(300)}
+            className="px-3 pb-2 flex flex-wrap flex-row justify-between items-center gap-2"
+          >
+            <TouchableOpacity onPress={handleSheetStore}>
+              <Text className="text-[18px] font-bold text-neutral-700">
+                {store.name}
+              </Text>
+              <Text className="text-sm font-semibold text-neutral-700">
+                Owner: {store.owner}
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={handleRefresh}
+              className="bg-blue-500 px-4 py-2 rounded-lg"
+            >
+              <Text className="text-white font-semibold text-[14px]">
+                <Icon name="refresh" size={14} color={"#ffffff"} /> Refresh Data
+              </Text>
+            </TouchableOpacity>
+          </Animated.View>
 
-        <Animated.View entering={FadeIn.delay(400)} className="flex flex-row justify-evenly items-center gap-2 pt-2 px-3">
-          <View className="bg-blue-300 w-[48%] rounded-lg p-4 shadow-lg">
-            <Text className="text-lg font-bold text-neutral-800">
-              {totalTransaction}
-            </Text>
-            <Text className="text-lg font-medium text-neutral-800">
-              Total Transaksi
-            </Text>
-          </View>
+          <Animated.View
+            entering={FadeIn.delay(400)}
+            className="flex flex-row justify-evenly items-center gap-2 pt-2 px-3"
+          >
+            <View className="bg-blue-300 w-[48%] rounded-lg p-4 shadow-lg">
+              <Text className="text-lg font-bold text-neutral-800">
+                {totalTransaction}
+              </Text>
+              <Text className="text-lg font-medium text-neutral-800">
+                Total Transaksi
+              </Text>
+            </View>
 
-          <View className="bg-blue-300 w-[48%] rounded-lg p-4 shadow-lg">
-            <Text className="text-lg font-bold text-neutral-800">
-              Rp {totalProfit ? totalProfit.toLocaleString("id-ID") : 0}
-            </Text>
-            <Text className="text-lg font-medium text-neutral-800">
-              Total Profit
-            </Text>
-          </View>
-        </Animated.View>
+            <View className="bg-blue-300 w-[48%] rounded-lg p-4 shadow-lg">
+              <Text className="text-lg font-bold text-neutral-800">
+                Rp {totalProfit ? totalProfit.toLocaleString("id-ID") : 0}
+              </Text>
+              <Text className="text-lg font-medium text-neutral-800">
+                Total Profit
+              </Text>
+            </View>
+          </Animated.View>
 
-        <Animated.View entering={FadeIn.delay(500)} className="my-4 px-3">
-          <Text className="text-start text-lg font-bold mb-2 text-neutral-800">Statistik Selama 7 Hari</Text>
-        <View className="flex justify-center items-center shadow-lg ">
-          {weeklyTransactions.length > 0 && (
-            <LineChart
-            style={{ borderRadius: 10 }}
-              data={data}
-              width={screenWidth - 20}
-              height={220}
-              chartConfig={chartConfig}
-              bezier
-              />
-            )}
-        </View>
-            </Animated.View>
+          <Animated.View entering={FadeIn.delay(500)} className="my-4 px-3">
+            <Text className="text-start text-lg font-bold mb-2 text-neutral-800">
+              Statistik Selama 7 Hari
+            </Text>
+            <View className="flex justify-center items-center shadow-lg ">
+              {weeklyTransactions.length > 0 && (
+                <LineChart
+                  style={{ borderRadius: 10 }}
+                  data={data}
+                  width={screenWidth - 20}
+                  height={220}
+                  chartConfig={chartConfig}
+                  bezier
+                />
+              )}
+            </View>
+          </Animated.View>
 
-        <Animated.View entering={FadeIn.delay(700)} className="px-3 pb-[100px]">
-        <Text className="text-start text-lg font-bold mb-2 text-neutral-800">Transaksi Hari Ini</Text>
+          <Animated.View
+            entering={FadeIn.delay(700)}
+            className="px-3 pb-[100px]"
+          >
+            <Text className="text-start text-lg font-bold mb-2 text-neutral-800">
+              Transaksi Hari Ini
+            </Text>
             <ScrollView horizontal>
               <View>
                 {/* Header Tabel */}
                 <View className="flex-row bg-gray-200 p-2 border-b border-gray-400">
-                  <Text className="w-[180px] text-center font-bold">Kode Transaksi</Text>
+                  <Text className="w-[180px] text-center font-bold">
+                    Kode Transaksi
+                  </Text>
                   <Text className="w-40 text-center font-bold">Produk</Text>
                   <Text className="w-24 text-center font-bold">Jumlah</Text>
-                  <Text className="w-32 text-center font-bold">Total Harga</Text>
+                  <Text className="w-32 text-center font-bold">
+                    Total Harga
+                  </Text>
                   <Text className="w-48 text-center font-bold">Timestamp</Text>
                 </View>
 
-                {
-                  transactionsArray.length > 0 ? (
-                    transactionsArray.map((group, groupIndex) => (
-                      group.map((transaction, index) => (
-                        <View
-                          key={`${groupIndex}-${index}`}
-                          className={`flex-row p-2 ${
-                            groupIndex % 2 === 0 ? "bg-white" : "bg-gray-100"
-                          }`}
-                        >
-                          <Text className="w-[180px] text-center">
-                            {transaction.codeTransaction}
-                          </Text>
-                          <Text className="w-40 text-center">
-                            {transaction.productName}
-                          </Text>
-                          <Text className="w-24 text-center">
-                            {transaction.quantity}
-                          </Text>
-                          <Text className="w-32 text-center">
-                            Rp {transaction.totalPrice.toLocaleString("id-ID")}
-                          </Text>
-                          <Text className="w-48 text-center">
-                            {transaction.timestamps}
-                          </Text>
-                        </View>
-                      ))
-                    ))     
-                  ) : (
-                    <View className="pb-4 rounded-lg bg-white flex justify-center items-center">
-                <Image source={require("../../assets/notFound.png")} alt="Tidak Ada Transaksi" className="w-72 h-72" />
-                <Text className="font-bold text-neutral-700 text-xl">Tidak Ada Produk...</Text>
-              </View>
-                  )               
-                }
+                {transactionsArray.length > 0 ? (
+                  transactionsArray.map((group, groupIndex) =>
+                    group.map((transaction, index) => (
+                      <View
+                        key={`${groupIndex}-${index}`}
+                        className={`flex-row p-2 ${
+                          groupIndex % 2 === 0 ? "bg-white" : "bg-gray-100"
+                        }`}
+                      >
+                        <Text className="w-[180px] text-center">
+                          {transaction.codeTransaction}
+                        </Text>
+                        <Text className="w-40 text-center">
+                          {transaction.productName}
+                        </Text>
+                        <Text className="w-24 text-center">
+                          {transaction.quantity}
+                        </Text>
+                        <Text className="w-32 text-center">
+                          Rp {transaction.totalPrice.toLocaleString("id-ID")}
+                        </Text>
+                        <Text className="w-48 text-center">
+                          {transaction.timestamps}
+                        </Text>
+                      </View>
+                    ))
+                  )
+                ) : (
+                  <View className="pb-4 rounded-lg bg-white flex justify-center items-center">
+                    <Image
+                      source={require("../../assets/notFound.png")}
+                      alt="Tidak Ada Transaksi"
+                      className="w-72 h-72"
+                    />
+                    <Text className="font-bold text-neutral-700 text-xl">
+                      Tidak Ada Produk...
+                    </Text>
+                  </View>
+                )}
               </View>
             </ScrollView>
-        </Animated.View>
+          </Animated.View>
 
-        {/* <Image source={} className="w-40 h-40" /> */}
-        {/* {
+          {/* <Image source={} className="w-40 h-40" /> */}
+          {/* {
           products.map((product) => {
             return (
               <View className="bg-white p-4 rounded-lg shadow-lg">
@@ -314,11 +332,15 @@ export default function HomeScreen() {
             )
           })
         } */}
-      </View>
-
-    </ScrollView>
-      <BottomSheetUpdateStore db={db} refRBSheet={refRbSheet} store={store} setStore={setStore} />
+        </View>
+      </ScrollView>
+      <BottomSheetUpdateStore
+        db={db}
+        refRBSheet={refRbSheet}
+        store={store}
+        setStore={setStore}
+      />
       <BottomNavigation path="Home" />
-      </>
+    </>
   );
 }
